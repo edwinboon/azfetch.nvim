9169fc1dc5e40f35d8568f7d4d7304502db91dc0
Edwin Boon <hello@edwinboon.dev>
2025-04-25 08:59:01 +0200

    WIP working on push function

--- lua/azure/push.lua
+++ lua/azure/push.lua
@@ -1,4 +1,31 @@
 local M = {}
 
 -- Function to push app settings to Azure Function App
-function M.push_app_settings() end
+function M.push_app_settings()
+	-- Prompt the user for the Azure Function App name
+	local app_name = vim.fn.input("Enter Azure Function App name: ")
+	if app_name == "" then
+		print("App name cannot be empty!")
+		return
+	end
+
+	-- Prompt the user for the Azure Function App resource group
+	local resource_group = vim.fn.input("Enter Azure Function App resource group: ")
+	if resource_group == "" then
+		print("Resource group cannot be empty!")
+		return
+	end
+
+	-- Get the current working directory
+	local cwd = vim.fn.getcwd()
+	local settings_file = cwd .. "/local.settings.json"
+
+	-- Read the local settings file
+	local file = io.open(settings_file, "r")
+	if not file then
+		print("Could not open local.settings.json in " .. cwd)
+		return
+	end
+
+	-- Parse the JSON content of the file
+end
